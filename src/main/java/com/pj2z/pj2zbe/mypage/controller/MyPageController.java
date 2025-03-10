10e857777e44b66946023c7e9608a15f1dee7c07
kyungtaeChoi <kmam147@gmail.com>
2025-03-11 02:06:39 +0900

    fix : test관련 코드 제거

--- src/main/java/com/pj2z/pj2zbe/mypage/controller/MyPageController.java
+++ src/main/java/com/pj2z/pj2zbe/mypage/controller/MyPageController.java
@@ -3,7 +3,6 @@ package com.pj2z.pj2zbe.mypage.controller;
 import com.pj2z.pj2zbe.mypage.dto.MyPageDto;
 import com.pj2z.pj2zbe.mypage.dto.MyPageResponseDto;
 import com.pj2z.pj2zbe.mypage.service.MyPageService;
-import com.pj2z.pj2zbe.test.dto.TestResponseDto;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
