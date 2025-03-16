38a9e014f5bb41c3be0a2339a97741cab908a174
shinheekim <dev.shinheekim@gmail.com>
2025-03-16 15:01:55 +0900

    rename: OpenAIConfig를 common 패키지로 이동(#13)

--- src/main/java/com/pj2z/pj2zbe/common/config/OpenAIConfig.java
+++ src/main/java/com/pj2z/pj2zbe/common/config/OpenAIConfig.java
@@ -1,4 +1,4 @@
-package com.pj2z.pj2zbe.recommend.config;
+package com.pj2z.pj2zbe.common.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
