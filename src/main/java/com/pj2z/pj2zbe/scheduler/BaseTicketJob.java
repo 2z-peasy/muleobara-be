0001a6d139f584f650a7c22efb41311b279c7c9b
shinheekim <dev.shinheekim@gmail.com>
2025-03-16 23:17:23 +0900

    chore: transactional 추가 및 log 추가(#14)

--- src/main/java/com/pj2z/pj2zbe/scheduler/BaseTicketJob.java
+++ src/main/java/com/pj2z/pj2zbe/scheduler/BaseTicketJob.java
@@ -2,12 +2,15 @@ package com.pj2z.pj2zbe.scheduler;
 
 import com.pj2z.pj2zbe.user.entity.User;
 import com.pj2z.pj2zbe.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Slf4j(topic = "BaseTicketJob")
 @Component
 @RequiredArgsConstructor
 public class BaseTicketJob {
@@ -15,9 +18,16 @@ public class BaseTicketJob {
     private final UserRepository userRepository;
 
     @Scheduled(cron = "0 0 0 1 * *")
+    @Transactional
     public void baseTicketUpdate(){
-        List<User> users = userRepository.findAll();
-        users.forEach(user -> user.setBaseTicket(5));
-        userRepository.saveAll(users);
+        try{
+            List<User> users = userRepository.findAll();
+            users.forEach(user -> user.setBaseTicket(5));
+            userRepository.saveAll(users);
+
+            log.info("Base ticket updated successfully: {}", users.size());
+        } catch (Exception e) {
+            log.error("Base ticket update failed: {}", e.getMessage());
+        }
     }
 }
