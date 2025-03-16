ff235b3a47d4204a22c648d6cb2b3b81bfe4348b
shinheekim <dev.shinheekim@gmail.com>
2025-03-16 17:40:10 +0900

    chore: 질문할 때 userId를 안넣으면 validation에서 걸리도록 수정(#13)

--- src/main/java/com/pj2z/pj2zbe/recommend/dto/request/RecommendRequest.java
+++ src/main/java/com/pj2z/pj2zbe/recommend/dto/request/RecommendRequest.java
@@ -1,10 +1,12 @@
 package com.pj2z.pj2zbe.recommend.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 public record RecommendRequest(
+        @NotNull
         Long userId,
         String setting,
         @NotBlank
