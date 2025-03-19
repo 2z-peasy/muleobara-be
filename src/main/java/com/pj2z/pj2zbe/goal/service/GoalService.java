f8f61bc3ecbffcd5488e4e8244d618c3e4fe1a8c
kyungtaeChoi <kmam147@gmail.com>
2025-03-19 21:58:28 +0900

    refactor: GoalService의 updateUserGoals메소드 소규모모듈회 (#16)

--- src/main/java/com/pj2z/pj2zbe/goal/service/GoalService.java
+++ src/main/java/com/pj2z/pj2zbe/goal/service/GoalService.java
@@ -23,22 +23,32 @@ public class GoalService {
 
     private final UserRepository userRepository;
 
-
-
     public void updateUserGoals(Long userId, List<String> goalNames) {
         // 1. 사용자와 관련된 기존 목표 목록을 가져옴
         User user = userRepository.findById(userId)
                     .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
-
-        Optional<List<UserGoal>> optionalGoals = userGoalRepository.findAllByUserId(userId);
-
-        List<UserGoal> existingGoals = optionalGoals.orElse(new ArrayList<>());
+        List<UserGoal> existingGoals = userGoalRepository.findAllByUserId(userId).orElse(new ArrayList<>());
 
         // 2. 기존 목표 중에 삭제해야 할 목표를 찾기
+        this.DeleteUserNotExistGoals(user,goalNames,existingGoals);
+
+        // 3. 새로 추가해야 할 목표를 찾아 추가
+        this.insertUserGoals(user,goalNames,existingGoals);
+    }
+
+    public void DeleteUserNotExistGoals(User user, List<String> goalNames) {
+        List<UserGoal> existingGoals = userGoalRepository.findAllByUserId(user.getId()).orElse(new ArrayList<>());
+        this.DeleteUserNotExistGoals(user,goalNames,existingGoals);
+    }
+
+    public void DeleteUserNotExistGoals(User user, List<String> goalNames,List<UserGoal> existingGoals) {
         Set<String> goalNamesSet = new HashSet<>(goalNames);
         Iterator<UserGoal> iterator = existingGoals.iterator();
         while (iterator.hasNext()) {
             UserGoal userGoal = iterator.next();
+            if(!user.getId().equals(userGoal.getUser().getId())) {
+                continue;
+            }
             String existingGoalName = userGoal.getGoal().getGoalName();
             if (!goalNamesSet.contains(existingGoalName)) {
                 // 새로 온 목표 목록에 없으면 삭제
@@ -46,8 +56,14 @@ public class GoalService {
                 iterator.remove();
             }
         }
+    }
 
-        // 3. 새로 추가해야 할 목표를 찾아 추가
+    //User로 한이유는 검증을 거치고 온 경우만 중복검증하기에 너무 과하다 판단
+    public void insertUserGoals(User user, List<String> goalNames){
+        List<UserGoal> existingGoals = userGoalRepository.findAllByUserId(user.getId()).orElse(new ArrayList<>());
+        this.insertUserGoals(user,goalNames,existingGoals);
+    }
+    public void insertUserGoals(User user, List<String> goalNames,List<UserGoal> existingGoals){
         for (String goalName : goalNames) {
             boolean isGoalExist = existingGoals.stream()
                     .anyMatch(userGoal -> userGoal.getGoal().getGoalName().equals(goalName));
