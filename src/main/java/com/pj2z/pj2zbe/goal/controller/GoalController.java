2f3d91d9c05f940d80d1cf1de84957cf3d3de106
shinheekim <dev.shinheekim@gmail.com>
2025-03-23 23:28:37 +0900

    chore: 메서드 네임 변경

--- src/main/java/com/pj2z/pj2zbe/goal/controller/GoalController.java
+++ src/main/java/com/pj2z/pj2zbe/goal/controller/GoalController.java
@@ -5,19 +5,15 @@ import com.pj2z.pj2zbe.goal.dto.GoalResponseDto;
 import com.pj2z.pj2zbe.goal.dto.GoalUpdateDto;
 import com.pj2z.pj2zbe.goal.dto.GoalYNUpdateDto;
 import com.pj2z.pj2zbe.goal.service.GoalService;
-import com.pj2z.pj2zbe.user.entity.User;
 import com.pj2z.pj2zbe.user.entity.UserGoalYN;
 import com.pj2z.pj2zbe.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
@@ -38,7 +34,7 @@ public class GoalController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body(Map.of("message", "로그인 정보가 유효하지 않습니다."));
         }
-        Long userId = Long.valueOf(jwtUtil.getUsernameFromToken(token));
+        Long userId = jwtUtil.getUserIdFromToken(token);
 
         try {
             goalService.updateUserGoals(userId, updateDto.getGoals());
@@ -61,7 +57,7 @@ public class GoalController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body(Map.of("message", "로그인 정보가 유효하지 않습니다."));
         }
-        Long userId = Long.valueOf(jwtUtil.getUsernameFromToken(token));
+        Long userId = jwtUtil.getUserIdFromToken(token);
 
         try {
             GoalResponseDto goalResponseDto = goalService.getGoalTotalDataByUserId(userId);
@@ -83,7 +79,7 @@ public class GoalController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body(Map.of("message", "로그인 정보가 유효하지 않습니다."));
         }
-        Long userId = Long.valueOf(jwtUtil.getUsernameFromToken(token));
+        Long userId = jwtUtil.getUserIdFromToken(token);
 
          try {
              userService.updateUserGoalYN(userId, goalUsedYN.isGoalYN() ? UserGoalYN.Y : UserGoalYN.N);
