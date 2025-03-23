905d14a54bac8fdfaf484b97e94e2ab61199c8ac
shinheekim <dev.shinheekim@gmail.com>
2025-03-24 01:22:06 +0900

    feat: MyPageController @UserCheck 수정(#27)
    - User에 대한 검증을 이미 resolver에서 진행하기 때문에 코드 수정

--- src/main/java/com/pj2z/pj2zbe/mypage/controller/MyPageController.java
+++ src/main/java/com/pj2z/pj2zbe/mypage/controller/MyPageController.java
@@ -1,15 +1,13 @@
 package com.pj2z.pj2zbe.mypage.controller;
 
-import com.pj2z.pj2zbe.mypage.dto.MyPageDto;
+import com.pj2z.pj2zbe.common.custom.UserCheck;
 import com.pj2z.pj2zbe.mypage.dto.MyPageResponseDto;
 import com.pj2z.pj2zbe.mypage.service.MyPageService;
-import jakarta.persistence.EntityNotFoundException;
+import com.pj2z.pj2zbe.user.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.sql.Timestamp;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyPageController {
@@ -17,8 +15,8 @@ public class MyPageController {
     MyPageService myPageService;
 
     @GetMapping("/mypage")
-    public ResponseEntity<MyPageResponseDto> getNickname(@RequestBody MyPageDto requestDto) {
-        MyPageResponseDto response = myPageService.getUserNickname(requestDto.getUserId());
+    public ResponseEntity<MyPageResponseDto> getNickname(@UserCheck User user) {
+        MyPageResponseDto response = myPageService.getUserNickname(user);
         return ResponseEntity.ok(response);
     }
 
