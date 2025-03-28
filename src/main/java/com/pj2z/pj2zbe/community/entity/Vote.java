bcf7a7ff1486f427d00a06389a389c8409b1d896
shinheekim <dev.shinheekim@gmail.com>
2025-03-28 17:50:19 +0900

    feat: Vote entity 생성(#33)

---
+++ src/main/java/com/pj2z/pj2zbe/community/entity/Vote.java
@@ -0,0 +1,42 @@
+package com.pj2z.pj2zbe.community.entity;
+
+import com.pj2z.pj2zbe.common.entity.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Vote extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long postId;
+
+    private String voteA;
+    private Integer voteACount = 0;
+
+    private String voteB;
+    private Integer voteBCount = 0;
+
+    private LocalDateTime voteDeadline;
+
+    public Vote(Long postId, String voteA, String voteB, LocalDateTime voteDeadline) {
+        this.postId = postId;
+        this.voteA = voteA;
+        this.voteB = voteB;
+        this.voteDeadline = voteDeadline;
+    }
+
+    public static Vote createVote(Long postId, String voteA, String voteB, LocalDateTime voteDeadline) {
+        return new Vote(postId, voteA, voteB, voteDeadline);
+    }
+}
