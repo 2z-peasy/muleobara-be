3b49853ec2fb49edb23200e63dedaa6966620656
shinheekim <dev.shinheekim@gmail.com>
2025-03-28 17:50:11 +0900

    feat: Post entity 생성(#33)

---
+++ src/main/java/com/pj2z/pj2zbe/community/entity/Post.java
@@ -0,0 +1,34 @@
+package com.pj2z.pj2zbe.community.entity;
+
+import com.pj2z.pj2zbe.common.entity.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Post extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+    private Long authorId;
+    // 공감, 댓글, 투표 등 존재
+
+    public Post(String title, String content, Long authorId) {
+        this.title = title;
+        this.content = content;
+        this.authorId = authorId;
+    }
+
+    public static Post createPost(String title, String content, Long authorId) {
+        return new Post(title, content, authorId);
+    }
+}
