94647a07312327744dad470e8e65480a08824a30
shinheekim <dev.shinheekim@gmail.com>
2025-05-11 19:51:12 +0900

    feat: like 누를 시 like count 수정 (#37)

--- src/main/java/com/pj2z/pj2zbe/community/service/LikeService.java
+++ src/main/java/com/pj2z/pj2zbe/community/service/LikeService.java
@@ -2,6 +2,7 @@ package com.pj2z.pj2zbe.community.service;
 
 import com.pj2z.pj2zbe.community.dto.LikeResponse;
 import com.pj2z.pj2zbe.community.entity.Likes;
+import com.pj2z.pj2zbe.community.entity.Post;
 import com.pj2z.pj2zbe.community.repository.LikeRepository;
 import com.pj2z.pj2zbe.community.repository.PostRepository;
 import com.pj2z.pj2zbe.user.entity.User;
@@ -18,22 +19,25 @@ public class LikeService {
 
     @Transactional
     public LikeResponse likePost(User user, Long postId) {
-        validatePostExists(postId);
+        Post post = validatePostExists(postId);
 
         if (!likeRepository.existsByPostIdAndUserId(postId, user.getId())) {
             Likes likes = new Likes(postId, user.getId());
+            post.updateLikeCount(1);
             likeRepository.save(likes);
+            postRepository.save(post);
         } else {
+            post.updateLikeCount(-1);
             likeRepository.deleteByPostIdAndUserId(postId, user.getId()); // 이미 공감을 눌렀다면 취소
+            postRepository.save(post);
         }
 
         Integer likeCount = likeRepository.countByPostId(postId);
         return new LikeResponse(likeCount);
     }
 
-    private void validatePostExists(Long postId) {
-        if (!postRepository.existsById(postId)) {
-            throw new IllegalArgumentException("해당 게시글이 없습니다.");
-        }
+    private Post validatePostExists(Long postId) {
+        return postRepository.findById(postId)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다."));
     }
 }
