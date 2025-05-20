9a555df0e3e6c17e98077355649b7e3350c5e431
kyungtaeChoi <kmam147@gmail.com>
2025-05-21 01:05:14 +0900

    feat: 코드 오타 제어를 위해 NotificationCode 명시 (#57)

--- src/main/java/com/pj2z/pj2zbe/notification/entity/enums/NotificationCode.java
+++ src/main/java/com/pj2z/pj2zbe/notification/entity/enums/NotificationCode.java
@@ -2,5 +2,12 @@ package com.pj2z.pj2zbe.notification.entity.enums;
 
 //대표적으로 사용할 템플릿 코드 등록
 public enum NotificationCode {
+    TICKET_EMPTY, TICKET_REFILL,
+    MISSION_REFRESH,
+    COMMUNITY_VOTE_NEW, COMMUNITY_VOTE_REACTION,
+    COMMENT_NEW,
+    BALANCE_REMINDER,
+    SYSTEM_UPDATE, SYSTEM_MAINTENANCE,
+    EVENT_STARTED
 
 }
---
+++ src/main/java/com/pj2z/pj2zbe/notification/service/NotificationService.java
@@ -0,0 +1,4 @@
+package com.pj2z.pj2zbe.notification.service;
+
+public class NotificationService {
+}
